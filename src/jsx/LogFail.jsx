82859fe5eb97da6126567ae955554fe2671be8be
wutaotao2018 <wttwtt2010@163.com>
2018-05-10 15:20:35 +0800

    modify

--- src/jsx/LogFail.jsx
+++ src/jsx/LogFail.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import history from '../history';
 
 export default class LogFail extends React.Component {
 
+    handleClick() {
+        history.push('/');
+    }
     render() {
-        return <div>登录失败！</div>;
+        return (<div>
+            <h3>登录失败！</h3>
+            <div><a onClick={this.handleClick}>返回登录</a></div>
+        </div>
+        );
     }
 }
